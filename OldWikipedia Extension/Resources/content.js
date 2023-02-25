279d8e47f124d7efac8455f6347a3861cad4f794
Benjamin Lucas <bnlcas@gmail.com>
2023-02-25 16:32:04 -0500

    publishing fixes

--- OldWikipedia Extension/Resources/content.js
+++ OldWikipedia Extension/Resources/content.js
@@ -1,36 +1,6 @@
 
 
-/*browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});*/
-
-console.log("Extension")
-
-/*
-function ammendWikipediaURL() {
-    console.log("append")
-    let url = window.location.href;
-    let oldVersionURL = "?useskin=vector"
-    //let targetSiteUrl = 'wikipedia.org'
-    //url.includes(targetSiteUrl) &&
-    if( !url.includes(oldVersionURL))
-    {
-        let oldVersionUrl = url + oldVersionURL;
-        console.log("redirected")
-        window.location.href = oldVersionUrl;
-        
-        //safari.extension.dispatchMessage('open', { url: oldVersionUrl });
-    }
-}
-
-safari.application.addEventListener("beforeNavigate", openHandler, true);
-function openHandler(e) { console.log(e); }
- */
-console.log("append")
 let url = window.location.href;
 let oldVersionURL = "?useskin=vector"
 //let targetSiteUrl = 'wikipedia.org'
@@ -39,12 +9,9 @@ if(!url.includes(oldVersionURL))
     let oldVersionUrl = url + oldVersionURL;
     console.log(oldVersionUrl)
     window.location.href = oldVersionUrl;
-    
-    //safari.extension.dispatchMessage('open', { url: oldVersionUrl });
 }
 
 window.addEventListener("load", (e) => {
-    console.log("append")
     let url = window.location.href;
     let oldVersionURL = "?useskin=vector"
     //let targetSiteUrl = 'wikipedia.org'
@@ -52,9 +19,7 @@ window.addEventListener("load", (e) => {
     {
         let oldVersionUrl = url + oldVersionURL;
         console.log(oldVersionUrl)
-        window.location.href = oldVersionUrl;
-        
-        //safari.extension.dispatchMessage('open', { url: oldVersionUrl });
+        window.location.href = oldVersionUrl;        
     }
 })
 
